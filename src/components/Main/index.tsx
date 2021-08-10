3b08be7902e83df3b90217860e6ee6f0ed0c55e8
Daniel Alves <daniel.alves@skytef.intranet>
2021-08-10 13:53:41 -0300

    Finalização do boilerplate com storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,18 +1,22 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+     title="React Avançado",
+     description=" Typescript, ReactJS, NextJS, e Styled Components"
+
+    }) => (
     <S.Wrapper>
         <S.Logo 
             src="/img/logo.svg"
             alt="Imagem de um átomo e React Avançado escrito ao lado."
         />
         <S.Title>
-            Typescript, ReactJS, NextJS, e Styled Components
+            {title}
         </S.Title>
         <S.Illustration 
             src="/img/hero-illustration.svg"
         />
-        <S.Description>React Avançado</S.Description>
+        <S.Description>{description}</S.Description>
     </S.Wrapper>
 )
 
